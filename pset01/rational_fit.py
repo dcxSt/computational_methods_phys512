72b3351b4f236f39a286171da9b3340646df8c6b
Stephen Fay <dcxstephen@live.co.uk>
2022-09-12 19:34:31 -0400

    vectorized rational fit

--- pset01/rational_fit.py
+++ pset01/rational_fit.py
@@ -5,10 +5,34 @@ n=3
 m=3
 fun=np.sin
 
+# def rational(coeffs,x,n=n,m=m):
+#     """x is a float"""
+#     numerator = np.dot(coeffs[:n],np.array([x**i for i in range(n)]))
+#     denom = 1 + np.dot(coeffs[n:],np.array([x**i for i in range(1,m)]))
+#     return numerator/denom
+
+
+# Evaluate x at the rational function with those coeffs
 def rational(coeffs,x,n=n,m=m):
-    """x is a float"""
-    numerator = np.dot(coeffs[:n],np.array([x**i for i in range(n)]))
-    denom = 1 + np.dot(coeffs[n:],np.array([x**i for i in range(1,m)]))
+    """Evaluates the rational function p(x)/(1+qq(x))
+
+    coeffs : ndarray
+        The coefficients of p(x) and qq(x)  
+
+    x : ndarray or float/int
+        What we are evaluating.
+
+    n : int
+        The number of numerator coeffs
+
+    m : int
+        The number of denom, non-constant coeffs
+    """
+    assert coeffs.shape==(n+m-1,)
+    num_coeffs = coeffs[:n]
+    denom_coeffs = np.hstack([[1],coeffs[n:]])
+    numerator = np.vstack([x**i for i in range(n)]).T@num_coeffs
+    denom = np.vstack([x**i for i in range(m)]).T@denom_coeffs
     return numerator/denom
 
 x=np.linspace(0,3,n+m-1)
@@ -35,7 +59,8 @@ print(f"INFO: hurray we got coeffs {coeffs}")
 print(rational(coeffs,0.0))
 
 xfine=np.linspace(0,3,1000)
-yfine=np.array([rational(coeffs,i) for i in xfine])
+yfine=rational(coeffs,xfine)
+# yfine=np.array([rational(coeffs,i) for i in xfine])
 ytrue=fun(xfine)
 
 plt.figure()
