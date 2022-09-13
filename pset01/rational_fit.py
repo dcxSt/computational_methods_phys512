574b6e4e95e8f030c35313ac61a9a660ab196615
Stephen Fay <dcxstephen@live.co.uk>
2022-09-12 21:13:48 -0400

    refactored rational fit into method, refactored module

--- pset01/rational_fit.py
+++ pset01/rational_fit.py
@@ -1,104 +1,103 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
-# Define some constants
-pi=np.pi
-
-# Rational function fit hyper-parameters
-n=3
-m=3
-# The function we are fitting
-fun=np.cos
-funcname="cos" # This is for the plot
-
-# def rational(coeffs,x,n=n,m=m):
-#     """x is a float"""
-#     numerator = np.dot(coeffs[:n],np.array([x**i for i in range(n)]))
-#     denom = 1 + np.dot(coeffs[n:],np.array([x**i for i in range(1,m)]))
-#     return numerator/denom
-
 
 # Evaluate x at the rational function with those coeffs
 def rational(coeffs,x,n=n,m=m):
     """Evaluates the rational function p(x)/(1+qq(x))
 
-    Does the same thing as rational ^^ above commented, but can take
-    vector intput x. 
-
     coeffs : ndarray
         The coefficients of p(x) and qq(x)  
 
     x : ndarray or float/int
-        What we are evaluating.
+        Data to evaluate.
 
     n : int
-        The number of numerator coeffs
+        Order of numerator polynomial.
 
     m : int
-        The number of denom, non-constant coeffs
+        Order of denomenator polynomial.
     """
     assert coeffs.shape==(n+m-1,) # Sanity check
-    num_coeffs = coeffs[:n]
-    denom_coeffs = np.hstack([[1],coeffs[n:]])
-    numerator = np.vstack([x**i for i in range(n)]).T@num_coeffs
-    denom = np.vstack([x**i for i in range(m)]).T@denom_coeffs
-    return numerator/denom
+    coeffs_numer = coeffs[:n]
+    coeffs_denom = np.hstack([[1],coeffs[n:]])
+    numer = np.vstack([x**i for i in range(n)]).T@coeffs_numer
+    denom = np.vstack([x**i for i in range(m)]).T@coeffs_denom
+    return numer/denom
+
+def rational_fit(x:np.ndarray, y:np.ndarray, n:int, m:int):
+    """Fit the data to a rational function using linear algebra
+
+    The rational fit will have an order n polynomial on the numerator
+    and an order m polynomial with constant term 1 on the denomenator.
+    """
+    # Sanity checks, easier debugging and *reading* (hint hint TA's)
+    assert x.shape==y.shape, "You're up to no good my friend"
+    assert x.shape==(n+m-1,), f"Too many sample points to fit, {x.shape} must match n+m-1={n+m-1}, try least squares fit instead."
+    # Build the part of the matrix corresponding to p(x)
+    mat_p=np.zeros((n+m-1,n))
+    for k in range(n):
+        mat_p[:,k] = x**k
+    # Build the part of the matrix corresponding to qq(x)
+    mat_qq=np.zeros((n+m-1,m-1))
+    for k in range(1,m):
+        mat_qq[:,k-1] = -y * (x**k)
+    # Stack the rectangular 'parts' to get a square matrix
+    mat=np.hstack((mat_p,mat_qq))
+    # Invert the matrices, solve for the coefficients
+    invmat=np.linalg.pinv(mat)
+    coeffs=invmat@y
+    return coeffs
+
+def polynomial_fit(x:np.ndarray, y:np.ndarray, ord:int):
+    """Fit the data to a polynomial using linear algebra"""
+    return 
 
 
-# Set a modest number of points
-x=np.linspace(-pi/2,pi/2,n+m-1)
-y=fun(x)
-# Finer resolution for evaluating the fit
-xfine=np.linspace(-pi/2,pi/2,1000)
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
 
-
-### Compute the coefficients
-# First, setup the coeffs matrix we need to invert
-# NB, even though we use for loops, we iterate over small ranges
-# The part of the matrix corresponding to p(x)
-mat_p=np.zeros((n+m-1,n))
-for k in range(n):
-    mat_p[:,k] = x**k
-
-# The part of the matrix corresponding to qq(x)
-mat_qq=np.zeros((n+m-1,m-1))
-for k in range(1,m):
-    mat_qq[:,k-1] = -y * (x**k)
-
-# Stack the 'componant' matrices to get a square matrix
-mat=np.hstack((mat_p,mat_qq))
-
-# Invert the matrices, solve for the coefficients
-invmat=np.linalg.pinv(mat)
-coeffs=invmat@y
-
-print(f"INFO: coeffs_p={coeffs[:n]}\ncoeffs_qq={coeffs[n:]}")
-
-# Use rational fit to eval, and get y_true for baseline
-y_rational=rational(coeffs,xfine)
-y_true=fun(xfine)
-
-### Plots 
-plt.subplots(1,2,figsize=(8,4))
-plt.subplot(1,2,1)
-plt.plot(x,y,"o",label="sample points")
-plt.plot(xfine,y_rational,label="best fit rational")
-plt.plot(xfine,ytrue,"--",label="truth")
-plt.legend()
-plt.xlabel("x")
-plt.ylabel(f"{funcname}(x)")
-plt.title(f"{funcname}--fits and data")
-# Residuals
-plt.subplot(1,2,2)
-plt.plot(xfine,y_rational-ytrue,label="y_rational-ytrue")
-plt.legend()
-plt.xlabel("x")
-plt.ylabel("Residuals")
-plt.title("Residuals")
-# Formatting stuff
-plt.tight_layout()
-plt.show(block=True)
-
-
+    # The function we are fitting
+    fun=np.cos
+    funcname="cos" # This is for the plot
+    
+    # Rational function order of numer and denom polynomials
+    n,m=3,3
+    ord=n+m-1 # Oder of fit / order of polynomial fit
+    
+    # Set a modest number of points
+    x=np.linspace(-np.pi/2,np.pi/2,ord)
+    y=fun(x)
+    
+    # Fit a rational function 
+    coeffs_rational=rational_fit(x,y,n,m)
+    print(f"DEBUG: Rational coeffs numerator {coeffs_rational[:n]}")
+    print(f"DEBUG: Rational coeffs denomenator {coeffs_rational[n:]}")
+    
+    # Sample finer resolution for evaluating the fits
+    xfine=np.linspace(-np.pi/2,np.pi/2,1000)
+    # Use rational fit to eval, and get y_true for baseline
+    y_rational=rational(coeffs_rational,xfine,n,m)
+    y_true=fun(xfine)
+    
+    ### Plots 
+    plt.subplots(1,2,figsize=(8,4))
+    plt.subplot(1,2,1)
+    plt.plot(x,y,"o",label="sample points")
+    plt.plot(xfine,y_rational,label="best fit rational")
+    plt.plot(xfine,ytrue,"--",label="truth")
+    plt.legend()
+    plt.xlabel("x")
+    plt.ylabel(f"{funcname}(x)")
+    plt.title(f"{funcname}--fits and data")
+    # Residuals
+    plt.subplot(1,2,2)
+    plt.plot(xfine,y_rational-ytrue,label="y_rational-ytrue")
+    plt.legend()
+    plt.xlabel("x")
+    plt.ylabel("Residuals")
+    plt.title("Residuals")
+    # Formatting stuff
+    plt.tight_layout()
+    plt.show(block=True)
+    
 
 
