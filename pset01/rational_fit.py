0b05f2f690c49e3a8c42700441c411f8bf062d70
Stephen Fay <dcxstephen@live.co.uk>
2022-09-12 20:37:17 -0400

    rational fit plots

--- pset01/rational_fit.py
+++ pset01/rational_fit.py
@@ -1,9 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Define some constants
+pi=np.pi
+
+# Rational function fit hyper-parameters
 n=3
 m=3
-fun=np.sin
+# The function we are fitting
+fun=np.cos
+funcname="cos" # This is for the plot
 
 # def rational(coeffs,x,n=n,m=m):
 #     """x is a float"""
@@ -15,6 +21,9 @@ fun=np.sin
 # Evaluate x at the rational function with those coeffs
 def rational(coeffs,x,n=n,m=m):
     """Evaluates the rational function p(x)/(1+qq(x))
+
+    Does the same thing as rational ^^ above commented, but can take
+    vector intput x. 
 
     coeffs : ndarray
         The coefficients of p(x) and qq(x)  
@@ -28,47 +37,66 @@ def rational(coeffs,x,n=n,m=m):
     m : int
         The number of denom, non-constant coeffs
     """
-    assert coeffs.shape==(n+m-1,)
+    assert coeffs.shape==(n+m-1,) # Sanity check
     num_coeffs = coeffs[:n]
     denom_coeffs = np.hstack([[1],coeffs[n:]])
     numerator = np.vstack([x**i for i in range(n)]).T@num_coeffs
     denom = np.vstack([x**i for i in range(m)]).T@denom_coeffs
     return numerator/denom
 
-x=np.linspace(0,3,n+m-1)
+
+# Set a modest number of points
+x=np.linspace(-pi/2,pi/2,n+m-1)
 y=fun(x)
+# Finer resolution for evaluating the fit
+xfine=np.linspace(-pi/2,pi/2,1000)
 
-matleft=np.zeros((n+m-1,n))
-for rowidx in range(n+m-1):
-    for entry in range(n):
-        matleft[rowidx,entry] = x[rowidx]**entry
 
-matright=np.zeros((n+m-1,m-1))
-for rowidx in range(n+m-1):
-    for entry in range(1,m):
-        matright[rowidx,entry-1] = -y[rowidx]*x[rowidx]**entry
+### Compute the coefficients
+# First, setup the coeffs matrix we need to invert
+# NB, even though we use for loops, we iterate over small ranges
+# The part of the matrix corresponding to p(x)
+mat_p=np.zeros((n+m-1,n))
+for k in range(n):
+    mat_p[:,k] = x**k
 
-mat=np.hstack((matleft,matright))
-# print(mat.shape)
-# print(mat)
+# The part of the matrix corresponding to qq(x)
+mat_qq=np.zeros((n+m-1,m-1))
+for k in range(1,m):
+    mat_qq[:,k-1] = -y * (x**k)
 
+# Stack the 'componant' matrices to get a square matrix
+mat=np.hstack((mat_p,mat_qq))
+
+# Invert the matrices, solve for the coefficients
 invmat=np.linalg.pinv(mat)
-
 coeffs=invmat@y
-print(f"INFO: hurray we got coeffs {coeffs}")
-print(rational(coeffs,0.0))
 
-xfine=np.linspace(0,3,1000)
-yfine=rational(coeffs,xfine)
-# yfine=np.array([rational(coeffs,i) for i in xfine])
-ytrue=fun(xfine)
+print(f"INFO: coeffs_p={coeffs[:n]}\ncoeffs_qq={coeffs[n:]}")
 
-plt.figure()
+# Use rational fit to eval, and get y_true for baseline
+y_rational=rational(coeffs,xfine)
+y_true=fun(xfine)
+
+### Plots 
+plt.subplots(1,2,figsize=(8,4))
+plt.subplot(1,2,1)
 plt.plot(x,y,"o",label="sample points")
-plt.plot(xfine,yfine,label="best-fit")
+plt.plot(xfine,y_rational,label="best fit rational")
 plt.plot(xfine,ytrue,"--",label="truth")
 plt.legend()
-# plt.plot(x,rational(coeffs,x))
+plt.xlabel("x")
+plt.ylabel(f"{funcname}(x)")
+plt.title(f"{funcname}--fits and data")
+# Residuals
+plt.subplot(1,2,2)
+plt.plot(xfine,y_rational-ytrue,label="y_rational-ytrue")
+plt.legend()
+plt.xlabel("x")
+plt.ylabel("Residuals")
+plt.title("Residuals")
+# Formatting stuff
+plt.tight_layout()
 plt.show(block=True)
 
 
