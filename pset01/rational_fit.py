69d64ae39e33f60e35f7c1c0947ffcba5905098c
Stephen Fay <dcxstephen@live.co.uk>
2022-09-12 21:51:04 -0400

    polynomial fit

--- pset01/rational_fit.py
+++ pset01/rational_fit.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# Evaluate x at the rational function with those coeffs
 def rational(coeffs,x,n=n,m=m):
     """Evaluates the rational function p(x)/(1+qq(x))
 
@@ -23,6 +22,25 @@ def rational(coeffs,x,n=n,m=m):
     denom = np.vstack([x**i for i in range(m)]).T@coeffs_denom
     return numer/denom
 
+def polynomial(coeffs,x):
+    """Evaluates the polynomial function w/ coefficients `coeffs` at x
+    
+    coeffs : ndarray
+        The ordered coefficients of the polynomial, from x^0 up.
+
+    x : ndarray or float/int
+        Data to evaluate
+
+    Returns
+    -------
+    ndarray or float/int
+        p(x) = 
+    """
+    ord=coeffs.shape[0]-1 # -1 ugly but necessary for consistancy
+    y=np.vstack([x**k for k in range(ord+1)]).T@coeffs
+    return y
+
+
 def rational_fit(x:np.ndarray, y:np.ndarray, n:int, m:int):
     """Fit the data to a rational function using linear algebra
 
@@ -43,13 +61,35 @@ def rational_fit(x:np.ndarray, y:np.ndarray, n:int, m:int):
     # Stack the rectangular 'parts' to get a square matrix
     mat=np.hstack((mat_p,mat_qq))
     # Invert the matrices, solve for the coefficients
-    invmat=np.linalg.pinv(mat)
-    coeffs=invmat@y
+    inv=np.linalg.pinv(mat)
+    coeffs=inv@y
     return coeffs
 
 def polynomial_fit(x:np.ndarray, y:np.ndarray, ord:int):
-    """Fit the data to a polynomial using linear algebra"""
-    return 
+    """Fit the data to a polynomial using linear algebra
+
+    Parameters
+    ----------
+    x : np.ndarray
+        The x data, must be 1d array with shape=(ord+1,)
+    y : np.ndarray
+        The y data, must be 1d array with shape=(ord+1,)
+    ord : int
+        The order of the polynomial we are fitting exactly to the data
+
+    Returns
+    -------
+    np.ndarray
+        Coefficients. ndarray with shape=(ord+1,)
+        i.e. P(x)=np.dot(coeffs,[x**k for k in range(ord+1)])
+    """
+    assert x.shape==y.shape
+    assert x.shape==(ord+1,)
+    mat=np.zeros((ord+1,ord+1))
+    for k in range(ord+1): mat[:,k]=x**k
+    inv=np.linalg.pinv(mat)
+    coeffs=inv@y
+    return coeffs
 
 
 if __name__ == "__main__":
@@ -60,29 +100,35 @@ if __name__ == "__main__":
     funcname="cos" # This is for the plot
     
     # Rational function order of numer and denom polynomials
-    n,m=3,3
-    ord=n+m-1 # Oder of fit / order of polynomial fit
+    n,m=6,5
+    ord=n+m-2 # Order of polynomial fit
+    # Number of degrees of freedom / number of coeffs to fit is ord+1
     
     # Set a modest number of points
-    x=np.linspace(-np.pi/2,np.pi/2,ord)
+    x=np.linspace(-np.pi/2,np.pi/2,ord+1)
     y=fun(x)
     
     # Fit a rational function 
     coeffs_rational=rational_fit(x,y,n,m)
     print(f"DEBUG: Rational coeffs numerator {coeffs_rational[:n]}")
     print(f"DEBUG: Rational coeffs denomenator {coeffs_rational[n:]}")
+    # Fit a polynomial function
+    coeffs_poly=polynomial_fit(x,y,ord)
     
     # Sample finer resolution for evaluating the fits
     xfine=np.linspace(-np.pi/2,np.pi/2,1000)
     # Use rational fit to eval, and get y_true for baseline
     y_rational=rational(coeffs_rational,xfine,n,m)
+    y_poly=polynomial(coeffs_poly,xfine)
     y_true=fun(xfine)
     
     ### Plots 
     plt.subplots(1,2,figsize=(8,4))
+    # Plot data and fits
     plt.subplot(1,2,1)
     plt.plot(x,y,"o",label="sample points")
     plt.plot(xfine,y_rational,label="best fit rational")
+    plt.plot(xfine,y_poly,label="best fit polynomial")
     plt.plot(xfine,ytrue,"--",label="truth")
     plt.legend()
     plt.xlabel("x")
@@ -91,6 +137,7 @@ if __name__ == "__main__":
     # Residuals
     plt.subplot(1,2,2)
     plt.plot(xfine,y_rational-ytrue,label="y_rational-ytrue")
+    plt.plot(xfine,y_poly-ytrue,label="y_poly-ytrue")
     plt.legend()
     plt.xlabel("x")
     plt.ylabel("Residuals")
