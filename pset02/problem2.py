03366448f87d29c370e3cadf2bc92c1e43c01d8d
Stephen Fay <dcxstephen@live.co.uk>
2022-09-22 15:17:18 -0400

    problem 2

---
+++ pset02/problem2.py
@@ -0,0 +1,98 @@
+"""
+One can work out the electric field from an infinitessimally thin spherical shell of charge with radius R by working out the field from a ring along its central axis, and integrating those rings to form a spherical shell. Use both your integrator and scipy.integrate.quad to plot the electric field from the shell as a function of distance from the center of the sphere. Make sure the range of your plot covers regions with z < R and z > R. Make sure one of your z values is R. Is there a singularity in the integral? Does quad care? Does your integrator? Note - if you get stuck setting up the problem, you may be able to find solutions to Griffiths problem 2.7, which sets up the integral.
+"""
+
+import numpy as np
+from scipy.integrate import quad
+pi=np.pi
+sqrt=np.sqrt
+cos=np.cos
+arctan=np.arctan
+
+
+def integrate(fun,a,b,tol,extra=None,rec_level=0):
+    """Integrates fun over the interval [a,b] with adaptive 5-point integral
+
+    fun : function
+        The function of one variable to integrate over
+    a : float
+        Lower bound
+    b : float
+        Upper bound
+    tol : float
+        Tolerance
+    extra : np.ndarray
+        An array of the three function evaluations that we have already made.
+    rec_level : int
+        The reccursion level. This is used to terminate the program without
+        crashing if there is a singularity. The max reccursion level is hard
+        coded to 10 for simplicity.
+    """
+    # At the first reccursion level, extra is None, sample 5 points
+    x=np.linspace(a,b,5)
+    dx=x[1]-x[0]
+    if extra is None:
+        y=fun(x) #Evaluate at all 5 points
+    else:
+        y=np.empty(5) # Init y array to be empty
+        y[::2]=extra  # Fill out what we already know about y
+        y[1::2]=fun(x[1::2]) # Only evaluate points we haven't already evaluated
+    i1=(y[0]+4*y[2]+y[4])/3*(2*dx) # Simpson's rule, 3point integral
+    i2=(y[0]+4*y[1]+2*y[2]+4*y[3]+y[4])/3*dx # 5point integral
+    myerr=np.abs(i1-i2)
+    if myerr<tol:
+        return i2
+    elif rec_level>=10:
+        print("WARNING: recursion level 10 has been reached, stopping here.")
+        print(f"WARNiNG: Error is still too large, myerr={myerr}")
+        return i2
+    else:
+        # Reccusion step
+        mid=(a+b)/2
+        # Extra is the first three steps (first half)
+        int1=integrate(fun,a,mid,tol/2,y[:3],rec_level+1) 
+        # Extra is the final three steps (latter half) (there is overlap on purpose!)
+        int2=integrate(fun,mid,b,tol/2,y[2:],rec_level+1)
+        return int1+int2
+
+
+"""main"""
+# Set up the integral
+R=1      #Radius of the sphere, (charge density is assumed to be 1)
+tol=0.01 #Error Tolerance
+xarr=np.arange(0,8,1/4) #Distances from center 0 to evaluate integral at
+result_integrate=[] #Stores the values returned by our integrator
+result_quad=[]      #Stores the values returned by scipy's integrator 
+# Integrate over y for each distance that we care about
+for x in xarr:
+    def fun(y,R=R,x=x): 
+        return 2*pi*sqrt(R**2-(x-y)**2)/(R**2+y**2)*cos(arctan(sqrt(R**2-(x-y)**2)/y))
+    a,b=x-R,x+R
+    print(f"DEBUG: x{x}, R{R}, a{a}, b{b}")
+    #Apply Custom integrator
+    result_integrate.append(integrate(fun,a,b,tol))
+    #Apply Scipy's Fortran 'quadrature' integrator
+    result_quad.append(quad(fun,a,b)[0])
+
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(8,6))
+
+plt.plot(xarr,result_quad,label="Scipy.integrate.quad")
+plt.plot(xarr,result_integrate,"--",label=f"Custom integrate, tolerance={tol}")
+plt.xlabel("Distance from center")
+plt.ylabel("Electric field")
+plt.title(f"Numerical Integration uniformly charged sphere R={R}")
+plt.legend()
+plt.tight_layout()
+# plt.savefig("img/nintegrate.png")
+
+plt.show()
+
+
+
+    
+
+
+
+
